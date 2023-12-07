7cb3093bb03d6b2a532eea9777cd3e9d60b6183e
guilhermerubrae <guilhermerubrae@gmail.com>
2023-12-07 02:25:12 -0300

    up

---
+++ Java/src/javaproject/Java01.java
@@ -0,0 +1,30 @@
+package javaproject;
+
+import java.util.Scanner;
+
+public class Java01 {
+
+public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+
+		String nome="Guilherme Rubrae";
+		int idade= 21;
+		float altura= (float) 1.81;
+		
+		System.out.println("\nMeu nome é: " +nome);
+		System.out.println("\nEu tenho: " +idade+" anos de vida");
+		System.out.println("\nEu tenho: " +altura+" metros de altura");
+	
+	
+		System.out.println("\n Entre com Nome: ");
+		nome = leia.nextLine();
+		System.out.println("\n Entre com Idade: ");
+		idade = leia.nextInt();
+		System.out.println("\n Entre com Altura: ");
+		altura = leia.nextFloat();
+		
+		leia.close();
+	}
+
+}
